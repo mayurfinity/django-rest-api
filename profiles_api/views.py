38a9ed0d2484322b57b430e5dc9e5c573c1791a8
Mayur <mayur.finity@gmail.com>
2021-02-12 21:00:05 +0530

    creating the profile api serizlizer

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -5,6 +5,7 @@ from rest_framework import viewsets
 
 
 from profiles_api import serializers
+from profiles_api import models
 
 class HelloApiView(APIView):
     """ test Api View"""
@@ -94,3 +95,9 @@ class HelloViewSet(viewsets.ViewSet):
         """ Handle remove an object """
         return Response({'http_method':'DELETE'})
 
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """ Handle creating and updating  profile """
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    
